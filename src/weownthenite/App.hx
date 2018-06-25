e726c52a9e6b58d8102e625583145f6bb41b0e89
tong <tong@disktree.net>
2018-06-25 13:40:36 +0200

    ...

--- src/weownthenite/App.hx
+++ src/weownthenite/App.hx
@@ -11,14 +11,12 @@ class App {
 
 		window.onload = function() {
 
-			var title = document.querySelector( 'header h1' );
-
-			document.fonts.ready.then( function(_){
-				title.style.display = 'block';
-			});
+			var container = document.querySelector( 'we-own-the-nite' );
+			var title = container.querySelector( 'h1.title' );
+			var footer = document.body.querySelector( 'footer' );
 
 			var video = document.createVideoElement();
-			//video.classList.add( 'background' );
+			video.classList.add( 'background' );
 			video.src = 'video/background.mp4';
 			video.volume = 0.0;
 			video.loop = true;
@@ -28,12 +26,47 @@ class App {
 				video.style.opacity = '1';
 				video.play();
 			}
-			document.body.appendChild( video );
+			container.appendChild( video );
 
-			window.oncontextmenu = e -> e.preventDefault();
+			document.fonts.ready.then( function(_){
+				title.style.display = 'block';
+			});
+
+			document.body.onblur = function(e){
+				footer.style.opacity = '0';
+			}
+			document.body.onfocus = function(e){
+				footer.style.opacity = '0.7';
+			}
+
+			/*
+			var links = document.querySelectorAll('a');
+			for( link in links ) {
+				//console.debug( link );
+			}
+			//trace(links);
+			*/
+
+			window.oncontextmenu = e -> {
+				e.preventDefault();
+			}
+			/*
 			window.onbeforeunload = e -> {
 				return null;
-			};
+			}
+			*/
+
+			/*
+			window.onscroll = function(e){
+				var scrollPos = window.innerHeight+window.scrollY;
+				if( scrollPos >= document.body.scrollHeight ) {
+					document.querySelector( 'footer' ).classList.remove('hidden');
+				} else {
+					document.querySelector( 'footer' ).classList.add('hidden');
+				}
+				//trace(window.innerHeight+window.scrollY, document.body.scrollHeight);
+			}
+			*/
 		}
 	}
 
