6c74488181b6b0fbe91557c0bfb8f24530b2f6fd
tong <tong@disktree.net>
2018-11-12 17:07:31 +0100

    Remove unused res/ directory

--- src/weownthenite/App.hx
+++ src/weownthenite/App.hx
@@ -14,26 +14,11 @@ class App {
 			var container = document.querySelector( 'we-own-the-nite' );
 			var title = container.querySelector( 'h1.title' );
 			var footer = document.body.querySelector( 'footer' );
-
-			var video = document.createVideoElement();
-			video.classList.add( 'background' );
-			video.src = 'video/background.mp4';
-			video.volume = 0.0;
-			video.loop = true;
-			video.muted = true;
-			video.oncanplaythrough = e -> {
-				//video.style.display = 'block';
-				video.oncanplaythrough = null;
-				video.currentTime = Math.random()*15;
-				video.style.opacity = '1';
-				video.play();
-			}
-			container.appendChild( video );
+			var video = container.querySelector( 'video' );
 
 			document.fonts.ready.then( function(_){
 				title.style.display = 'block';
 			});
-
 			document.body.onblur = function(e){
 				//footer.style.opacity = '0';
 			}
@@ -41,34 +26,23 @@ class App {
 				//footer.style.opacity = '0.7';
 			}
 
+			window.oncontextmenu = e -> {
+				e.preventDefault();
+			}
+
+			video.oncanplaythrough = e -> {
+				video.oncanplaythrough = null;
+				video.style.opacity = '1';
+			}
+
 			/*
 			var links = document.querySelectorAll('a');
 			for( link in links ) {
 				//console.debug( link );
 			}
-			//trace(links);
 			*/
 
-			window.oncontextmenu = e -> {
-				e.preventDefault();
-			}
-			/*
-			window.onbeforeunload = e -> {
-				return null;
-			}
-			*/
 
-			/*
-			window.onscroll = function(e){
-				var scrollPos = window.innerHeight+window.scrollY;
-				if( scrollPos >= document.body.scrollHeight ) {
-					document.querySelector( 'footer' ).classList.remove('hidden');
-				} else {
-					document.querySelector( 'footer' ).classList.add('hidden');
-				}
-				//trace(window.innerHeight+window.scrollY, document.body.scrollHeight);
-			}
-			*/
 		}
 	}
 
