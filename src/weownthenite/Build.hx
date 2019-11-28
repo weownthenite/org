0de287264ad2dc2cd2ea86f8d8a9ec99168a8aba
tong <tong@disktree.net>
2019-11-28 10:19:19 +0100

    more strict video file names

--- src/weownthenite/Build.hx
+++ src/weownthenite/Build.hx
@@ -2,6 +2,7 @@ package weownthenite;
 
 #if macro
 import sys.FileSystem;
+using StringTools;
 #end
 
 class Build {
@@ -9,7 +10,10 @@ class Build {
 	macro public static function getVideoFiles() : ExprOf<Array<String>> {
 		var path = 'bin/video';
 		var arr = FileSystem.readDirectory( path ).filter( function(e){
-			return !FileSystem.isDirectory( '$path/$e' );
+			return
+				!e.startsWith( '.' )
+				&& !e.startsWith( '_' )
+				&& !FileSystem.isDirectory( '$path/$e' );
 		});
 		return macro $v{arr};
 	}
